204eac55e7569b143172d96763455c26593d8fdd
truongld1991 <leductruong09t2@gmail.com>
2021-01-18 21:03:54 +0700

    update

--- command/ytbtoolcommand/main.m
+++ command/ytbtoolcommand/main.m
@@ -6,7 +6,20 @@ int main(int argc, char *argv[], char *envp[]) {
 		NSString *command = @"none";
 		if (argc >= 1) {
 			command = [NSString stringWithUTF8String:argv[1]];
-			printf("%s", [command UTF8String]);
+			NSString *appName = @"AppName";
+			if (argc > 2) {
+				appName = [NSString stringWithUTF8String:argv[2]];
+				if([command isEqualToString:@"CloseApp"]) {
+						printf("Close AppName: %s\n", appName);
+						NSlog(@"Close AppName: %s\n", appName);
+					}
+				else if([day isEqualToString:@"Sun"]) {
+						NSlog(@"Somthing...");
+					}
+			} else {
+				printf("%s\n", [command UTF8String]);
+			}
+			
 		}else {
 			printf("No command!");
 		}
