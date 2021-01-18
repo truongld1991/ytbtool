838e1b4eccf5446a2e203cec35fb6829ce60258f
truongld1991 <leductruong09t2@gmail.com>
2021-01-18 20:35:25 +0700

    add fix

--- command/ytbtoolcommand/main.m
+++ command/ytbtoolcommand/main.m
@@ -2,7 +2,14 @@
 
 int main(int argc, char *argv[], char *envp[]) {
 	@autoreleasepool {
-		printf("Hello world!\n");
+		NSString *command = @"none";
+		int proxyMode = 0;	//0: no proxy;1: https proxy
+		if (argc >= 1) {
+			command = [NSString stringWithUTF8String:argv[1]];
+			printf("Command : " + command);
+		}else {
+			printf("No command!");
+		}
 		return 0;
 	}
 }
